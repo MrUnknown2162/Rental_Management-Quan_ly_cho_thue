4f9e4b0b24a948efe619edc9f800ff3b530c19ba
MrUnknown2162 <23050168@student.bdu.edu.vn>
2025-12-11 14:32:28 +0700

    Add JWT login API and authentication logic

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -5,6 +5,10 @@ from app.database.session import get_db
 from app.schemas.user import UserCreate, UserRead
 from app.crud.user import create_user, get_user_by_email
 
+from fastapi.security import OAuth2PasswordRequestForm
+from app.crud.user import authenticate_user
+from app.core.token import create_access_token
+
 router = APIRouter()
 
 @router.post("/register", response_model=UserRead)
@@ -15,3 +19,12 @@ def register(data: UserCreate, db: Session = Depends(get_db)):
     
     new_user = create_user(db, data)
     return new_user
+
+@router.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+
+    access_token = create_access_token({"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from app.models.user import User
 from app.schemas.user import UserCreate
 from app.core.security import hash_password
+from app.core.security import verify_password
 
 def create_user(db: Session, data: UserCreate):
     new_user = User(
@@ -16,3 +17,11 @@ def create_user(db: Session, data: UserCreate):
 
 def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = get_user_by_email(db, email)
+    if not user:
+        return None
+    if not verify_password(password, user.password_hash):
+        return None
+    return user
